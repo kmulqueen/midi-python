cb143de4cd158f31365b29bf1429800685824d8d
Kyle Mulqueen <kmulqdrums710@gmail.com>
2023-04-09 11:28:16 -0700

    show sine wave

--- 3_functions/music.py
+++ 3_functions/music.py
@@ -1,13 +1,12 @@
 import rtmidi
 import time
-from rtmidi.constants import *
+from midi.constants import *
 midi_out = rtmidi.MidiOut()
 midi_out.open_port(PORT)
 
 
 def send_notes(pitch=60, repeat=1):
     for note in range(repeat):
-        print(pitch)
         note_on = [CHANNEL_1, pitch, 50]
         note_off = [CHANNEL_2, pitch, 0]
         midi_out.send_message(note_on)
@@ -17,7 +16,7 @@ def send_notes(pitch=60, repeat=1):
 
 with midi_out:
     pitch = 60
-    for i in range(4):
-        send_notes(pitch + i)
+    for i in range(len(SCALES["major"])):
+        send_notes(pitch + SCALES["major"][i])
 
 
---
+++ 4_modulation_shapes/music.py
@@ -0,0 +1,39 @@
+import rtmidi
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from midi.constants import *
+midi_out = rtmidi.MidiOut()
+midi_out.open_port(PORT)
+
+
+def sine_waves(repeat=1):
+    """ Function that shows a lot of sine waves modulation shapes """
+    t = np.arange(0, 80, 0.1)  # Time
+    amplitude = np.cos(t)  # Height of wave at certain point
+    print(t)
+    print('amplitude', amplitude)
+    plt.plot(t, amplitude)  # Draw
+    plt.title("Modulation Shape")
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.show()  # Show
+
+def sine_waves_short(repeat=1):
+    """ Function that shows 1 sine wave modulation shape """
+    t = np.arange(0, 80, 0.1)  # Time
+    amplitude = np.cos(t)  # Height of wave at certain point
+
+    # Use list slicing to get smaller range of elements from time & amplitude
+    # The smaller the range the more zoomed in the image is
+    plt.plot(t[1:60], amplitude[1:60])  # Draw
+    plt.title("Modulation Shape")
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.grid(True, which="both")
+    plt.axhline(y=0)
+    plt.show()  # Show
+
+# sine_waves()
+sine_waves_short()
